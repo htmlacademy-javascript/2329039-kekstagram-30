93ce1656ca1fd2aebacb706e6bbdc518477c0bf4
Nick Strokin <nickstrokin1999@gmail.com>
2023-11-01 10:29:14 +0300

    Чистка кода

--- js/main.js
+++ js/main.js
@@ -36,28 +36,13 @@ const createIdGenerator = () => {
   };
 };
 const generateCommentId = createIdGenerator();
-
+const generatePhotoId = createIdGenerator();
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
-
-function createRandomIdFromRangeGenerator(min, max) {
-  const previousValues = [];
-  let currentValue = getRandomInteger(min, max);
-  if (previousValues.length >= (max - min + 1)) {
-    // eslint-disable-next-line no-console
-    console.error(`Перебраны все числа из диапозона от ${min} до ${max}`);
-    return null;
-  }
-  while (previousValues.includes(currentValue)) {
-    currentValue = getRandomInteger(min.max);
-  }
-  previousValues.push(currentValue);
-  return currentValue;
-}
 
 const getRandomArrayElement = (elements) =>
   elements[getRandomInteger(0, elements.length - 1)];
@@ -79,7 +64,7 @@ function getRandomCommentArray() {
 
 const createDescription = (index) => ({
   id: index + 1,
-  url: `photos/${createRandomIdFromRangeGenerator(1,25)}.jpg`,
+  url: `photos/${generatePhotoId()}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomInteger(5, 200),
   comments: getRandomCommentArray()
