d980ae93c794b5103c1d70f7b42e63c10b4f2b42
Nick Strokin <nickstrokin1999@gmail.com>
2023-11-01 22:41:41 +0300

    module5-task2

--- js/function.js
+++ js/function.js
@@ -1,1 +1,18 @@
+function itsTime(beginDay, endDay, beginMeeting, duration) {
+  let splitBegin = beginDay.split(':');
+  let splitEnd = endDay.split(':');
+  let splitMeeting = beginMeeting.split(':');
+  let beginInMinutes = splitBegin[0] * 60 + +splitBegin[1];
+  let endInMinutes = splitEnd[0] * 60 + +splitEnd[1];
+  let meetingInMinutes = splitMeeting[0] * 60 + +splitMeeting[1];
+  let difference = endInMinutes - meetingInMinutes;
+  if(beginInMinutes > meetingInMinutes) {
+    return false;
+  } else if(difference >= duration) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+itsTime('08:00', '15:00', '14:00', 60);
