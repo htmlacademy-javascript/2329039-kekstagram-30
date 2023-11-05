2785e005d4b32ba40b8b2d73416a89868794d17b
Nick Strokin <nickstrokin1999@gmail.com>
2023-11-05 11:32:08 +0300

    Правки

--- js/function.js
+++ js/function.js
@@ -1,18 +1,12 @@
-function itsTime(beginDay, endDay, beginMeeting, duration) {
-  let splitBegin = beginDay.split(':');
-  let splitEnd = endDay.split(':');
-  let splitMeeting = beginMeeting.split(':');
-  let beginInMinutes = splitBegin[0] * 60 + +splitBegin[1];
-  let endInMinutes = splitEnd[0] * 60 + +splitEnd[1];
-  let meetingInMinutes = splitMeeting[0] * 60 + +splitMeeting[1];
-  let difference = endInMinutes - meetingInMinutes;
-  if(beginInMinutes > meetingInMinutes) {
-    return false;
-  } else if(difference >= duration) {
-    return true;
-  } else {
-    return false;
-  }
+function determineTime(startDay, endDay, startMeeting, duration) {
+  const splitStart = startDay.split(':');
+  const splitEnd = endDay.split(':');
+  const splitMeeting = startMeeting.split(':');
+  const startInMinutes = splitStart[0] * 60 + +splitStart[1];
+  const endInMinutes = splitEnd[0] * 60 + +splitEnd[1];
+  const meetingInMinutes = splitMeeting[0] * 60 + +splitMeeting[1];
+  const difference = endInMinutes - meetingInMinutes;
+  return startInMinutes <= meetingInMinutes && difference >= duration;
 }
 
-itsTime('08:00', '15:00', '14:00', 60);
+determineTime('08:00', '15:00', '14:00', 60);
